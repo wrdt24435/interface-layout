3067e60a7e5eae5bbfbe94d4e61527121b9ab23d
wrdt24435 <55676237+wrdt24435@users.noreply.github.com>
2019-12-04 07:32:06 +0800

    done
    完成

--- js/my.js
+++ js/my.js
@@ -88,6 +88,7 @@ const data = [
       ( function (window,document) {
           let my_box = function ({data, Select}) {
               this.types = ['王菲'];  // 所有的分类,
+              this.current_type;
               this.classified = {'王菲': []}; // 按照类型分类后的图片
               this.all_img_element = [];	//所有图片元素集合
               //this.curType = initType; // 当前显示的图片分类
@@ -134,37 +135,60 @@ const data = [
           }
           //第一次显示
           my_box.prototype.init_view = function(Select) {
+              this.current_type = Select;
               let head_box = [];
               for(let type of this.types) {
                   let select_class = type == Select? " select" : "";
-                  head_box.push(`<div class="title${select_class}">${type}<div>`);
+                  head_box.push(`<div class="title${select_class}">${type}</div>`);
               }
               $(".head").append(...head_box);
               //console.log(this.classified[Select]);			
               this.img_view(Select);
           }
+          
+          my_box.prototype.change_head = function(Select) {
+              $(".head div").removeClass("select");
+              let i = 0;  
+              for(let type of this.types) {
+                  if (type == Select) {
+                      $(`.head div:eq(${i})`).addClass('select');
+                  }
+                  //head_box.push(`<div class="title${select_class}">${type}<div>`);
+                  i++
+              }
+              this.img_view(Select);
+              this.bind_event();
+          }
           //显示head盒子对应的内容
           my_box.prototype.img_view = function(Select) {
               let img = [];
-              let $box = $(".content")
-              //console.log($box);
+              let $box = $(".content");
+              //setTimeout( () => {
+              $box.empty();
+                  //console.log($box);
               for(let img_element of this.classified[Select]) {
                       $box.append(this.all_img_element[img_element]);
                       //console.log(this.all_img_element[img_element]);
               }
-              $(".content img").css("width","240px");
-              //$box.append($('<p>test</p>'));
+              $(".content img").css("width","0");
+              //$(".content img").css("width","240px");
+              setTimeout( () => {
+                  $(".content img").css("width","240px");
+              })
+              //}, 280)
+              //$(".content img").css("width","240px");
           }
           my_box.prototype.bind_event = function() {
-              let $content_figure = $(".content figure");
-              let $big_picture = $(".big_picture");
+              let $content_figure = $(".content figure"); //g
+              let $big_picture = $(".big_picture"); //g
               $content_figure.click( (obj) => {
                   let my_src = obj.target.firstElementChild.getAttribute("src");
-                  //console.log(`${my_src}`);
-                 $(".big_picture img").attr("src",`${my_src}`);
+                  //console.log($content_figure.index(obj.target));
+                  let img_index = $content_figure.index(obj.target) //g 获取元素在$对象的索引
+                  let $big_img = $(".big_picture img"); //g
+                  $big_img.attr("src",`${my_src}`);
                   $big_picture.css("display","flex");
                   setTimeout( () => { $big_picture.css("opacity","1"); });//跑完事件函数后才能显示动画
-                  //console.log($(".big_picture img"));
                   $big_picture.click( () => {
                       $big_picture.css("opacity","0");
   
@@ -172,6 +196,27 @@ const data = [
                           $big_picture.css("display","none");
                       }, 300);
                   });
+                  $big_picture.children()[0].onclick = (event) => { //上一张事件
+                      if (img_index) {
+                          img_index--;
+                      }
+                      let arr_index = this.classified[this.current_type][img_index];  //获取classified对应下标
+                      let src = this.all_img_element[img_index].children()[0].getAttribute("src");  //获取下标元素的src
+                      $big_img.attr("src",`${src}`);
+                      event.stopPropagation();
+                  }
+                  $big_picture.children()[2].onclick = (event) => { //下一张事件
+                      if (img_index < this.classified[this.current_type].length - 1) {
+                          img_index++;
+                      }
+                      let src = this.all_img_element[img_index].children()[0].getAttribute("src");
+                      $big_img.attr("src",`${src}`);
+                      event.stopPropagation();
+                  }
+              })
+              let $title = $(".head .title");  //g
+              $title.click( (event) => {
+                this.change_head(event.target.textContent);
               })
               //$(".big_picture img").css("src","images/1.jpg");
               //console.log( $(".big_picture div") );
